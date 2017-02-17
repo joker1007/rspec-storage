44644c6ecf28509eed34db87f0baa6a749898919
joker1007 <kakyoin.hierophant@gmail.com>
2017-02-17 21:24:59 +0900

    Add monkey_patch for RSpec::Core::Reporter
    in order to call `output#close` always

--- lib/rspec/storage/monkey_patch.rb
+++ lib/rspec/storage/monkey_patch.rb
@@ -1,6 +1,5 @@
 require "rspec/core/formatters"
-require "rspec/core/formatters/base_formatter"
-require "rspec/core/formatters/base_text_formatter"
+require "rspec/core/reporter"
 require "uri"
 
 module RSpec
@@ -21,7 +20,20 @@ module RSpec
         end
       end
     end
+
+    module ReporterExtension
+      def close
+        super
+        registered_listeners(:close).each do |formatter|
+          output = formatter.output
+          if output.respond_to?(:close) && output.respond_to?(:closed?)
+            output.close if !output.closed? && output != $stdout && output != $stderr
+          end
+        end
+      end
+    end
   end
 end
 
 RSpec::Core::Formatters::Loader.prepend(RSpec::Storage::FormattersLoaderExtension)
+RSpec::Core::Reporter.prepend(RSpec::Storage::ReporterExtension)
--- spec/rspec/storage_spec.rb
+++ spec/rspec/storage_spec.rb
@@ -4,4 +4,8 @@ describe Rspec::Storage do
   it "has a version number" do
     expect(Rspec::Storage::VERSION).not_to be nil
   end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
 end
