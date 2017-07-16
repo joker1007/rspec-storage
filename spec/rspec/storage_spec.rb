a86b5ff894e242a1804e9913f993d79ea892415c
abicky <takeshi.arabiki@gmail.com>
2017-07-17 00:10:20 +0900

    Support rspec-core 3.6.0
    `file_at` doesn't exist anymore in rspec-core 3.6.0.
    In addition, $stdout is wrapped with OutputWrapper
    and `output != $stdout` doesn't work anymore.
    cf. https://github.com/rspec/rspec-core/pull/2401

--- spec/rspec/storage_spec.rb
+++ spec/rspec/storage_spec.rb
@@ -5,7 +5,19 @@ describe Rspec::Storage do
     expect(Rspec::Storage::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context "with s3 scheme" do
+    it do
+      expect(RSpec::Storage::S3).to receive(:handle_location)
+      config = RSpec::Core::Configuration.new
+      config.add_formatter("json", "s3://example.com/result.json")
+    end
+  end
+
+  context "with gs scheme" do
+    it do
+      expect(RSpec::Storage::GCS).to receive(:handle_location)
+      config = RSpec::Core::Configuration.new
+      config.add_formatter("json", "gs://example.com/result.json")
+    end
   end
 end
